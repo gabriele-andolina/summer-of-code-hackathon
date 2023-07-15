b3e06bac8a1889218867860d8ab373e42dcb6b4c
gabriele-andolina <gabe.andolina@gmail.com>
2023-07-16 01:54:07 +0200

    Added markers for six events' trails

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,15 +57,45 @@ async function initMap() {
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
     map = new Map(document.getElementById("map"), {
-        zoom: 4,
+        zoom: 9,
         center: position,
         mapId: "DEMO_MAP_ID",
     });
 
-    const marker = new AdvancedMarkerElement({
-        map: map,
-        position: position,
-        title: "Uluru",
+    const ticknock = new AdvancedMarkerElement({
+        map,
+        position: { lat: 53.255716553795814, lng: -6.2510793852498505 },
+        title: "Ticknock",
+    });
+
+    const tibradden = new AdvancedMarkerElement({
+        map,
+        position: { lat: 53.242979844676746, lng: -6.294514045563979 },
+        title: "Tibradden",
+    });
+
+    const howthCliff = new AdvancedMarkerElement({
+        map,
+        position: { lat: 53.38961204534314, lng: -6.071991282868927 },
+        title: "Howth Cliff",
+    });
+
+    const djouce = new AdvancedMarkerElement({
+        map,
+        position: { lat: 53.15585306091994, lng: -6.188564839539633 },
+        title: "Djouce",
+    });
+
+    const sugarloaf = new AdvancedMarkerElement({
+        map,
+        position: { lat: 53.144508111702116, lng: -6.154635143215563 },
+        title: "Sugarloaf",
+    });
+
+    const ballinastoeForest = new AdvancedMarkerElement({
+        map,
+        position: { lat: 53.1026255627096, lng: -6.254810200706456 },
+        title: "Ballinastoe Forest",
     });
 }
 
