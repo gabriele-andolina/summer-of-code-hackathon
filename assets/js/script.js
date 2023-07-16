db655210dd080a28eb56fcc02732bec54e82d9f6
gabriele-andolina <gabe.andolina@gmail.com>
2023-07-16 02:06:37 +0200

    Included Places API in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@
             h ||
             (h = new Promise(async (f, n) => {
                 await (a = m.createElement("script"));
-                e.set("libraries", [...r] + "");
+                e.set("libraries", [...r] + "places");
                 for (k in g)
                     e.set(
                         k.replace(/[A-Z]/g, (t) => "_" + t[0].toLowerCase()),
